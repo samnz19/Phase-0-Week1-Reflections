3984783a737365efb80343ba372cb7eb87c9cc47
user <user@users.noreply.github.com>
2015-03-28 11:24:04 +1300

    first commit

---
+++ Practise Basics/Practise Basics/Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practise_Basics
+{
+    class Car
+    {
+        private string brand;
+
+        public Car(string newBrand)
+        {
+            brand = newBrand;
+        }
+
+        public void Drive()
+        {
+            Console.WriteLine(brand);
+            Console.WriteLine("Were Driving");
+            
+        }
+    }
+        
+}
---
+++ Practise Basics/Practise Basics/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practise_Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ListPritatice();
+            Car newCar = new Car("Toyota");
+            newCar.Drive();
+          
+
+            Car newCar2 = new Car("Audi");
+            newCar2.Drive();
+            Console.ReadLine();
+
+        }
+
+        //public static void ListPritatice()
+        //{
+        //    List<string> Words = new List<string>();
+        //    Words.Add("house");
+        //    Words.Add("boat");
+        //    Console.WriteLine(Words[1]);
+        //    Console.ReadLine();
+        //}
+    }
+}
+
